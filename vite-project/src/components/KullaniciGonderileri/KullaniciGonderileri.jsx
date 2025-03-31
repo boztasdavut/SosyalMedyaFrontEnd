85ed0a6dd294c37848332bc68400618ee92f768b
Davut Boztas <89689967+boztasdavut@users.noreply.github.com>
2025-03-31 05:26:19 +0300

    Gönderiler için detayları ayarlandı.

--- vite-project/src/components/KullaniciGonderileri/KullaniciGonderileri.jsx
+++ vite-project/src/components/KullaniciGonderileri/KullaniciGonderileri.jsx
@@ -3,9 +3,15 @@ import "./KullaniciGonderileri.css";
 import { tumGonderiler } from "../../services/KullaniciTumGonderileri";
 import emptyProfilePicture from "../../assests/empty.png";
 import { BsThreeDots } from "react-icons/bs";
+import { MdDelete, MdEditSquare } from "react-icons/md";
+import { IoStatsChartSharp } from "react-icons/io5";
 
 function KullaniciGonderileri() {
   const [kullaniciVerileri, setKullaniciVerileri] = useState([]);
+  const [gonderiAyarlariVisibility, setGonderiAyarlariVisibility] = useState(
+    {}
+  );
+
   useEffect(() => {
     const fetchData = async () => {
       const gelenVeri = await tumGonderiler();
@@ -13,27 +19,82 @@ function KullaniciGonderileri() {
     };
     fetchData();
   }, []);
+
+  // Ayar açma/kapama fonksiyonu
+  const gonderiAyarlariHandle = (gonderiId) => {
+    setGonderiAyarlariVisibility((prevState) => {
+      const newState = { ...prevState };
+      if (newState[gonderiId]) {
+        delete newState[gonderiId];
+      } else {
+        Object.keys(newState).forEach((key) => {
+          delete newState[key];
+        });
+        newState[gonderiId] = true;
+      }
+      return newState;
+    });
+  };
+
+  // Sayfa dışında tıklama kontrolü
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (!event.target.closest(".profilResmiIcerigiVeAyarlarDiv")) {
+        setGonderiAyarlariVisibility({});
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+    return () => document.removeEventListener("click", handleClickOutside);
+  }, []);
+
   return (
     <div className="kullanicininTumGonderileriAnaDiv">
-      {kullaniciVerileri.map((veri) => (
-        <div key={veri.gonderiId} className="profilResmiIcerigiVeAyarlarDiv">
-          <div className="profilResmiVeIcerigiDiv" key={veri.gonderiId}>
-            <div>
-              <img
-                className="anasayfaProfilResmi"
-                src={emptyProfilePicture}
-                alt="Logo"
-                width={60}
-                height={60}
+      {kullaniciVerileri.length === 0 ? (
+        <div className="gonderiBulunamadi">Herhangi gönderiniz bulunmuyor.</div>
+      ) : (
+        kullaniciVerileri.map((veri) => (
+          <div key={veri.gonderiId} className="profilResmiIcerigiVeAyarlarDiv">
+            <div className="profilResmiVeIcerigiDiv">
+              <div>
+                <img
+                  className="anasayfaProfilResmi"
+                  src={emptyProfilePicture}
+                  alt="Logo"
+                  width={60}
+                  height={60}
+                />
+              </div>
+              <div className="kullanicininPaylastigiGonderi">
+                {veri.gonderiIcerigi}
+              </div>
+            </div>
+            <div className="gonderiAyarlariIconDiv">
+              <BsThreeDots
+                id="gonderiAyarlari"
+                size={25}
+                onClick={() => gonderiAyarlariHandle(veri.gonderiId)}
               />
             </div>
-            <div className="kullanicininPaylastigiGonderi">{veri.gonderiIcerigi}</div>
+            {gonderiAyarlariVisibility[veri.gonderiId] && (
+              <div className="gonderiAyarlariDiv">
+                <div className="gönderiSilmeAnaDiv">
+                  <MdDelete />
+                  <div>Gönderiyi Sil</div>
+                </div>
+                <div className="gonderiyiDuzenlemeAnaDiv">
+                  <MdEditSquare />
+                  <div>Gönderiyi Düzenle</div>
+                </div>
+                <div className="gonderiIstatistikleriniGor">
+                  <IoStatsChartSharp />
+                  <div>Gönderi İstatistikleri</div>
+                </div>
+              </div>
+            )}
           </div>
-          <div>
-            <BsThreeDots size={25} />
-          </div>
-        </div>
-      ))}
+        ))
+      )}
     </div>
   );
 }
